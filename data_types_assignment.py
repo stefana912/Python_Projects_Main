cf2e7f36c912f1b165c919a4660e47bb60d3fb94
Stefana Muller <stefana912@gmail.com>
2020-05-12 11:42:34 -0400

    assignment - define 4 variables and print them to console

---
+++ data_types_assignment.py
@@ -0,0 +1,21 @@
+#data_types_assignment.py
+#Week 1 - Python Training
+#May 12, 2020
+#Stefana Muller
+
+"""Assignment: Define 4 variables
+10
+20.54
+True
+"I am the best"
+"""
+
+a=10
+b=20.54
+c=True
+d="I am the best"
+
+print(a)
+print(b)
+print(c)
+print(d)
