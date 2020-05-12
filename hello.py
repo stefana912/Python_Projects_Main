a56381599bfe1d339a9cef9b6b2fd192ce25c4c8
Stefana Muller <stefana912@gmail.com>
2020-05-12 10:26:53 -0400

    add all files in Python Training

---
+++ hello.py
@@ -0,0 +1,17 @@
+""" Author is Stefana Muller
+Created on May 11, 2020
+"""
+''' you can also use single quotes to comment
+multi-line comments
+'''
+#This is my first python program
+print('Python is easy!!')
+
+'''
+5 Data types in python
+None: an object aht doesn't contain value
+Numeric: int, float, complex
+Sequences: str, bytes, bytearray, list, tuple, range
+Sets: no duplicates
+Mappings: map and reduce
+'''
